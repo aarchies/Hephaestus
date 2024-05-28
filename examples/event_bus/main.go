c4e986bb6a95bb5cad446594501e0d4840872782
aarchies <2624784422@qq.com>
2024-05-28 21:05:55 +0800

    更新示例

--- examples/event_bus/main.go
+++ examples/event_bus/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/aarchies/go-lib/examples/event_bus/event"
 	"github.com/aarchies/go-lib/logs"
+	"github.com/aarchies/go-lib/messagec/cqrs"
 	"github.com/aarchies/go-lib/messagec/cqrs/contrib/kafka"
 	"github.com/sirupsen/logrus"
 	"sync"
@@ -12,23 +13,21 @@ import (
 
 func main() {
 	logs.AddLogsModule(logrus.DebugLevel)
+
 	var wg sync.WaitGroup
 	factory := kafka.NewConnectionFactory(kafka.NewConfig([]string{"ip:port", "ip:port", "ip:port"}, "clickId", "", ""))
-	c := kafka.NewEventBus(factory, 3)
-	//c := kafka.NewEventBusWithConfig(factory, cqrs.EventBusConfig{
-	//	Retry:           3,
-	//	OnPublishBefore: nil,
-	//	OnPublishAfter:  nil,
-	//	OnError: func(params cqrs.OnEventErrorParams) {
-	//		logrus.Errorln(params)
-	//	},
-	//	Marshaler: cqrs.JsonMarshaler{},
-	//})
+	//c := kafka.NewEventBus(factory, 3)
+	c := kafka.NewEventBusWithConfig(factory, cqrs.EventBusConfig{
+		Retry:           3,
+		OnPublishBefore: nil,
+		OnPublishAfter:  nil,
+		OnError: func(params cqrs.OnEventErrorParams) {
+			logrus.Errorln(params)
+		},
+		Marshaler: cqrs.JsonMarshaler{},
+	})
 
-	// 订阅
-	e := event.NewTestModel()
-
-	c.Subscribe(context.Background(), e, event.TestHandler{})
+	c.Subscribe(context.Background(), event.NewTestModel(), event.TestHandler{})
 
 	go func() {
 		for i := 0; i < 200; i++ {
@@ -38,10 +37,8 @@ func main() {
 	}()
 
 	time.Sleep(time.Second * 7)
-
-	// 取消订阅
 	c.UnSubscribe(event.NewTestModel())
 
-	wg.Add(3)
+	wg.Add(1)
 	wg.Wait()
 }
